114c6c735e7915e1c98de63a01cd0c293fbea9a1
AngelaDiSalvo <angela.disalvo@gmail.com>
2018-08-12 21:04:02 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Angela", last_name: 'D')
+
+Student.create(first_name: "Michelle", last_name: 'S')
